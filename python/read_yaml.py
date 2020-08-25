04697c604685a04a3368de6570009488cd4ca11e
Peter <Peter_Betlem@hotmail.com>
2020-08-25 13:12:39 +0200

    Added metashape workflow class

--- python/read_yaml.py
+++ python/read_yaml.py
@@ -14,22 +14,35 @@ import os
 # import specialised libs
 import yaml
 import pathlib
+import Metashape
 
-def convert_paths(a_dict):
+"""
+Based on UCDavis work, see https://github.com/ucdavis/metashape,
+Part below based on https://stackoverflow.com/a/25896596/237354
+
+"""
+
+def convert_paths_and_commands(a_dict):
     for k, v in a_dict.items():
         if not isinstance(v, dict):
             if isinstance(v, str):
                 if v and ('path' in k):    # all paths that are supplied in the config file are automatically converted to Path type
                     a_dict[k] = pathlib.Path(v)
+                elif v and 'Metashape' in v and not ('path' in k) and not ('project' in k): # for Metashape compatibility
+                    a_dict[k] = eval(v)
+                # TODO: add other commands that need eval
+                # elif v and 'Metashape' in v and not ('path' in k) and not ('project' in k): # for Metashape compatibility
+                #     a_dict[k] = eval(v)
+            elif isinstance(v, list):
+                a_dict[k] =  [eval(item) for item in v if("Metashape" in item)]
         else:
-            a_dict[k] = convert_paths(v)
+            a_dict[k] = convert_paths_and_commands(v)
     return a_dict
+
 
 def read_yaml(yml_path):
     yml_path = pathlib.Path(yml_path)
     with open(yml_path,'r') as ymlfile:
         cfg = yaml.load(ymlfile, Loader=yaml.SafeLoader)
-        
-    cfg = convert_paths(cfg)
-
-    return cfg
+         
+    return convert_paths_and_commands(cfg)
